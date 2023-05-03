1c33b7fd9d1855f234657edc92e29e26ea42d740
Lucas <lucas.santos517@fatec.sp.gov.br>
2023-05-03 09:28:57 -0300

    files

--- src/main/java/com/example/projeto/controller/HomeController.java
+++ src/main/java/com/example/projeto/controller/HomeController.java
@@ -9,32 +9,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-//    @GetMapping("/")
-//    public String home(ModelMap model) {
-//        model.addAttribute("nomeDoAtributo", "Teste");
-//        return "home";
-//    }
-
-    @GetMapping("/departamento/{nomeDep}")
+    @GetMapping("/{nomeDep}")
     public String departamento(@PathVariable String nomeDep, ModelMap model) {
         model.addAttribute("nome", nomeDep);
-        if(nomeDep == "livro"){
-            deps(nomeDep);
+        switch(nomeDep){
+            case "livro":
+                mainHome(nomeDep);
+                break;
+            case "login":
+                login(nomeDep);
+                break;
+            case "dashboard":
+                dashboard(nomeDep);
+                break;
+
+
         }
+//        if(nomeDep == "livro"){
+//            deps(nomeDep);
         return nomeDep;
-    }
+        }
+
+
 
     @GetMapping
-    public String deps(String t){
+    public String mainHome(String t){
         if(t == "livro"){
             return "livro";
-        } else if(t == "perfume"){
-            return "perfume";
+        }else{
+            return "home";
         }
-        return "home";
     }
 
+    public String login(String t){
+        if(t == "login"){
+            return "login";
+        }else{
+            return "home";
+        }
+    }
 
-
+    public String dashboard(String t){
+        if(t == "login"){
+            return "dashboard";
+        }else{
+            return "home";
+        }
+    }
 }
 
